ac80ff3c356297396cee5ff1aff34621519b1d33
GJ Tiquia <gershom.tiquia@gmail.com>
2023-01-31 10:18:08 +0800

    pause and resume button functionality

--- Assets/Scripts/GameSceneUIController.cs
+++ Assets/Scripts/GameSceneUIController.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class GameSceneUIController : MonoBehaviour
 {
+    [SerializeField] GameObject _pauseContainer;
+
     public void OnPauseButtonClicked()
     {
         Debug.Log("Pause clicked!");
+
+        _pauseContainer.SetActive(true);
     }
 
     public void OnResumeButtonClicked()
     {
         Debug.Log("Resume clicked!");
+
+        _pauseContainer.SetActive(false);
     }
 
     public void OnRestartButtonClicked()
